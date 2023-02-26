78213d9f3ff810774fec18a642998010af945dfd
stunnerarts <54142529+stunnerarts@users.noreply.github.com>
2023-02-26 08:34:21 -0500

    APP-MovieList-009: (1) Updated Search bar border

--- components/movieSearchBar.tsx
+++ components/movieSearchBar.tsx
@@ -16,18 +16,22 @@ const MovieSearchBar = (props: Props) => {
     >
       <label
         htmlFor="Search Movies by Title"
-        className="block text-3xl font-light text-gray-100"
+        className="block text-2xl font-light text-gray-100"
       >
         Search Movies by Title
       </label>
       <div className="relative mt-1 rounded-md shadow-sm ">
         <input
           className="  
-        h-full bg-gradient-to-r to-slate-800 from-slate-800 opacity-70 
+        h-full bg-gradient-to-r 
+        to-slate-800 from-slate-800 
+        opacity-70 
         text-indigo-100 font-light
         animate-pulse
-        w-full rounded-3xl border-gray-100 pl-7 pr-12 
-    focus:border-indigo-500 focus:ring-indigo-500 sm:text-xl p-3"
+        w-full rounded-3xl 
+      
+        pl-7 pr-12 
+        focus:border-indigo-500 focus:ring-indigo-500 sm:text-xl p-3"
           type="text"
           value={searchTerm}
           onChange={handleSearch}
