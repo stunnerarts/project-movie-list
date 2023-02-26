d0b264b19728bf5b3145ae183fb46be9c8aa8e9a
stunnerarts <54142529+stunnerarts@users.noreply.github.com>
2023-02-26 08:44:54 -0500

    APP-MovieList-010: (1) Adjusted the style for sortContainer and searchBarContainer

--- components/sort/sortButton.tsx
+++ components/sort/sortButton.tsx
@@ -11,7 +11,7 @@ function SortButton(props: Props) {
       onSortChange(sortOrder);
     };
   return (
-    <div className="flex justify-end mt-6 shadow-lg pb-5 pr-6 sortContainer">
+    <div className="flex justify-end mt-6 shadow-lg pb-5 pr-12 sortContainer">
       <button
         className="rounded-lg shadow-md
          bg-gradient-to-r from-indigo-900 to-indigo-800 
